78c5aefaa540b295d4d9a1679c5763c1ed96df2a
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-10-14 09:07:16 +0200

    Terminada versión JavaScript del tutorial 8

--- App.tsx
+++ App.tsx
@@ -1,59 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Image } from 'expo-image'
 import Futbolista from './components/Futbolista'
 
 export default function App() {
   return (
     <View style={styles.contenedor}>
-      <Image
-        source={require("./assets/campeones.png")}
-        style={styles.logo}
-        contentFit={"fill"}
+      <Futbolista
+        nombre={"Jose"}
+        dorsal={9}
+        puesto={"delantero"}
+        style={styles.gigante}
       />
-      <View style={styles.campo}>
-        <View style={styles.fila}>
-          <Futbolista nombre={"Casillas"} dorsal={1} puesto={"Portero"} colorFondo={"#a2d6eeff"} />
-        </View>
-        <View style={styles.fila}>
-          <Futbolista nombre={"Ramos"} dorsal={15} puesto={"Defensa"} />
-          <Futbolista nombre={"Piqué"} dorsal={3} puesto={"Defensa"} />
-          <Futbolista nombre={"Puyol"} dorsal={5} puesto={"Defensa"} />
-          <Futbolista nombre={"Capdevila"} dorsal={11} puesto={"Defensa"} />
-        </View>
-        <View style={styles.fila}>
-          <Futbolista nombre={"Busquets"} dorsal={16} puesto={"Centrocampista"} />
-          <Futbolista nombre={"Alonso"} dorsal={14} puesto={"Centrocampista"} />
-        </View>
-        <View style={styles.fila}>
-          <Futbolista nombre={"Xavi"} dorsal={8} puesto={"Centrocampista"} />
-          <Futbolista nombre={"Iniesta"} dorsal={6} puesto={"Centrocampista"} />
-          <Futbolista nombre={"Pedro"} dorsal={18} puesto={"Centrocampista"} />
-        </View>
-        <View style={styles.fila}>
-          <Futbolista nombre={"Villa"} dorsal={7} puesto={"Delantero"} />
-        </View>
-      </View>
+      <Futbolista
+        nombre={"Miguel"}
+        dorsal={11}
+        puesto={"delantero"}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   contenedor: {
-    flex: 1
-  },
-  logo: {
-    width: "100%",
-    height: 250
-  },
-  campo: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "#468944ff",
-    rowGap: 20
+    alignItems: "center"
   },
-  fila: {
-    flexDirection: "row",
-    justifyContent: "space-around"
+  gigante: {
+    width: 300,
+    height: 300,
+    marginTop: 200,
+    borderRadius: 30,
+    color: "green"
   }
 })
--- components/Futbolista.tsx
+++ components/Futbolista.tsx
@@ -1,18 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-type FutbolistaProps = {
-    nombre:string
-    dorsal:number
-    puesto:string
-    colorFondo?:string
-}
-
-export default function Futbolista({nombre,dorsal,puesto,colorFondo=styles.contenedor.backgroundColor}:FutbolistaProps) {
+export default function Futbolista({nombre,dorsal,puesto,style={}}) {
+  style = StyleSheet.flatten(style)
+  const estiloContenedorAmpliado = {
+    width:style.width ?? styles.contenedor.width,
+    height:style.height ?? styles.contenedor.height,
+    backgroundColor:style.backgroundColor ?? styles.contenedor.backgroundColor
+  }
+  const estiloDorsalAmpliado = {
+    color: style.color ?? styles.dorsal.color
+  }
   return (
-    <View style={[styles.contenedor, {backgroundColor:colorFondo}]}>
+    <View style={[styles.contenedor, estiloContenedorAmpliado]}>
       <Text style={styles.nombre}>{nombre}</Text>
-      <Text style={styles.dorsal}>{dorsal}</Text>
+      <Text style={[styles.dorsal, estiloDorsalAmpliado]}>{dorsal}</Text>
       <Text style={styles.puesto}>{puesto}</Text>
     </View>
   )
